0dda4cae9758134d81c3a992c7c912810117722e
Jameson Campbell <jcampbell@flexterapp.com>
2020-08-18 16:55:31 -0500

    removed user submited quote validation logic

--- src/App.js
+++ src/App.js
@@ -37,11 +37,6 @@ const App = () => {
   };
 
   const submitQuote = value => {
-
-    if (!value.name) {
-      alert('Please Enter a Quote')
-      return
-    }
     setData([...data, { id: data.length + 1, quote: value }])
     setValue('')
   }
